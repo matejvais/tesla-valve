cdbf8a9879036c69fed051355ded3b874947d022
Matěj Vais <maty.vais@gmail.com>
2024-05-16 17:45:48 +0200

    continuing with netgen

--- mesh.py
+++ mesh.py
@@ -43,9 +43,64 @@ def nanomesh(file_name):
     mesh.write('out.msh', file_format='gmsh22', binary=False)
 
 def netgen_mesh():
-    heights = np.array([1,8,10,20,21,33,37,41,53])
+    #heights = np.array([1,8,10,20,21,33,37,41,53])
     geo = SplineGeometry()
+    pnts = [
+     (0   ,-38),
+     (133 ,  0),
+     (189 ,-16),
+     (189 ,-24),
+     (195 ,-26),
+     (210 ,-53),
+     (181 ,-69),
+     ( 86 ,-37),
+     (129 ,-26),
+     (191 ,-37),
+     (197 ,-49),
+     (184 ,-57)]
+    
+def corner(pointA,pointB):
+    '''
+    Return the coordinates of the "corner" point used for creating a spline3 curve approximating a
+    quarter-circle.
+    '''
+    a1,a2,b1,b2 = pointA[0],pointA[1],pointB[0],pointB[1]
+    if b2 > a2:
+        c1, c1 = 0.5 * (a1 + b1 + b2 - a2), 0.5 * (a2 + b2 + a1 - b1)
+    else:
+        c1, c2 = 0.5 * (a1 + b1 + a2 - b2), 0.5 * (a2 + b2 + b1 - a1)
+    return tuple((c1, c2))
+
+
+def shift_coords(data, x_shift, y_shift):
+    '''
+    Prints coordinates of points together with their shifted counterparts.
+    '''
+    data = np.concatenate([data+np.array([x_shift, y_shift])], axis=1)
+    print(data)
 
 
 if __name__ == "__main__":
-    netgen_mesh()
+    
+    '''
+    data = np.array([
+        [123,-20],
+        [179,1],
+        [253,16],
+        [309,-1],
+        [309,-8],
+        [318,-10],
+        [333,-37],
+        [304,-53],
+        [209,-21],
+        [252,-10],
+        [314,-21],
+        [320,-33],
+        [307,-41]
+        ])
+    shift_coords(data, -123, -16)
+    '''
+    A, B = (195 ,-26), (210 ,-53)
+    C,D = (210 ,-53),(181 ,-69)
+
+    print(corner(C,D))
