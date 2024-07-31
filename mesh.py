5a64c3ece4c16c0fdfb195637b666c69fbf9a26c
Matěj Vais <maty.vais@gmail.com>
2024-07-31 10:47:28 +0200

    adding the inner hole in the lobe

--- mesh.py
+++ mesh.py
@@ -2,62 +2,98 @@ import PIL as pil
 import numpy as np
 #import nanomesh as nm
 from netgen.geom2d import SplineGeometry
+from firedrake import *
 
 
 
-def generate_bw(file_name):
-    '''
-    Generate a black and white image and save it.
-    '''
-    image = pil.Image.open(file_name)
-    image = image.convert('L') 
-    image = image.point(lambda i: i < 175 and 255)
-    image.save("bw-valve-figure.png")
+# BEGINNING OF NANOMESH FUNCTIONS
+#################################################
 
-def generate_array(file_name, save=False, display_np=False):
-    '''
-    Generate a numpy array from an image.
-    options:
-        save - saves the numpy array as a binary file
-        display_np - saves the visual representation of the numpy array as a image
-    '''
-    np_image = np.asarray(pil.Image.open(file_name))[:,:,0:3]
-    np_image = np.max(np_image, axis=2)
-    #print(np_image.shape)
-    if save:
-        np.save("np_image", np_image)
-    if display_np:
-        pil.Image.fromarray(np_image).save('visual_np_image.png')
+# def generate_bw(file_name):
+#     '''
+#     Generate a black and white image and save it.
+#     '''
+#     image = pil.Image.open(file_name)
+#     image = image.convert('L') 
+#     image = image.point(lambda i: i < 175 and 255)
+#     image.save("bw-valve-figure.png")
 
-def nanomesh(file_name):
-    '''
-    Generation of the mesh from a numpy array.
-    See the following website for details:
-        https://nanomesh.readthedocs.io/en/latest/examples/examples_generate_a_2d_triangular_mesh.html
-    '''
-    data = nm.Image.load(file_name)
-    mesher = nm.Mesher2D(data)
-    mesher.generate_contour(max_edge_dist=5)
-    mesh = mesher.triangulate(opts='q20a10')
-    #triangle_mesh = mesh.get('triangle')
-    mesh.write('out.msh', file_format='gmsh22', binary=False)
+# def generate_array(file_name, save=False, display_np=False):
+#     '''
+#     Generate a numpy array from an image.
+#     options:
+#         save - saves the numpy array as a binary file
+#         display_np - saves the visual representation of the numpy array as a image
+#     '''
+#     np_image = np.asarray(pil.Image.open(file_name))[:,:,0:3]
+#     np_image = np.max(np_image, axis=2)
+#     #print(np_image.shape)
+#     if save:
+#         np.save("np_image", np_image)
+#     if display_np:
+#         pil.Image.fromarray(np_image).save('visual_np_image.png')
+
+# def nanomesh(file_name):
+#     '''
+#     Generation of the mesh from a numpy array.
+#     See the following website for details:
+#         https://nanomesh.readthedocs.io/en/latest/examples/examples_generate_a_2d_triangular_mesh.html
+#     '''
+#     data = nm.Image.load(file_name)
+#     mesher = nm.Mesher2D(data)
+#     mesher.generate_contour(max_edge_dist=5)
+#     mesh = mesher.triangulate(opts='q20a10')
+#     #triangle_mesh = mesh.get('triangle')
+#     mesh.write('out.msh', file_format='gmsh22', binary=False)
+
+# END OF NANOMESH FUNCTIONS
+#################################################
+
+
 
 def netgen_mesh():
-    #heights = np.array([1,8,10,20,21,33,37,41,53])
     geo = SplineGeometry()
-    pnts = [
+    ps = [
      (0   ,-38),
      (133 ,  0),
      (189 ,-16),
      (189 ,-24),
      (195 ,-26),
      (210 ,-53),
-     (181 ,-69),
+     (181 ,-69)]
+    qs = [
      ( 86 ,-37),
      (129 ,-26),
      (191 ,-37),
      (197 ,-49),
      (184 ,-57)]
+    p0, p1, p2, p3, p4, p5, p6 = [geo.AppendPoint(*p) for p in ps]
+    p45 = geo.AppendPoint(*corner(ps[4], ps[5])) 
+    p56 = geo.AppendPoint(*corner(ps[5], ps[6]))
+    q0, q1, q2, q3, q4 = [geo.AppendPoint(*q) for q in qs]
+    q23 = geo.AppendPoint(*corner(qs[2], qs[3]))
+    q34 = geo.AppendPoint(*corner(qs[3], qs[4]))
+    outer_curves = [
+        [["line", p0, p6], "line"],
+        [["spline3", p6, p56, p5], "curve"],
+        [["spline3", p5, p45, p4], "curve"],
+        [["line", p4, p3], "line"],
+        [["line", p3, p2], "line"],
+        [["line", p2, p1], "line"],    
+        [["line", p1, p0], "line"] 
+    ]
+    inner_curves = [
+        [["line", q0, q1], "line"],
+        [["line", q1, q2], "line"],
+        [["spline3", q2, q23, q3], "curve"],
+        [["spline3", q3, q34, q4], "curve"],
+        [["line", q4, q0], "line"] 
+    ]
+    [geo.Append(c, bc=bc) for c, bc in outer_curves]
+    [geo.Append(c, bc=bc) for c, bc in inner_curves]
+    ngmsh = geo.GenerateMesh(maxh=10)
+    msh = Mesh(ngmsh)
+    VTKFile("output/MeshExample2.pvd").write(msh)
     
 def corner(pointA,pointB):
     '''
@@ -74,6 +110,7 @@ def corner(pointA,pointB):
 
 def shift_coords(data, x_shift, y_shift):
     '''
+    Auxiliary function.
     Prints coordinates of points together with their shifted counterparts.
     '''
     data = np.concatenate([data+np.array([x_shift, y_shift])], axis=1)
@@ -81,26 +118,4 @@ def shift_coords(data, x_shift, y_shift):
 
 
 if __name__ == "__main__":
-    
-    '''
-    data = np.array([
-        [123,-20],
-        [179,1],
-        [253,16],
-        [309,-1],
-        [309,-8],
-        [318,-10],
-        [333,-37],
-        [304,-53],
-        [209,-21],
-        [252,-10],
-        [314,-21],
-        [320,-33],
-        [307,-41]
-        ])
-    shift_coords(data, -123, -16)
-    '''
-    A, B = (195 ,-26), (210 ,-53)
-    C,D = (210 ,-53),(181 ,-69)
-
-    print(corner(C,D))
+    netgen_mesh()
